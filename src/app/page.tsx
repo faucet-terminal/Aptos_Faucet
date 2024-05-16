cdb2377e2cd9fc39cef4726438e0d7b711429253
finch ren <344919881@qq.com>
2024-05-17 00:34:04 +0800

    APTOS  FAUCET

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,76 @@
-import Image from "next/image";
+"use client"
+import { useState, useTransition } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [address, setAddress] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [isPending, startTransition] = useTransition()
+  const [result, setResult] = useState();
+
+
+  const handleClick = () => startTransition(async () => {
+    if (!address) {
+      return alert("请输入地址")
+    }
+    try {
+      const response = await fetch(`/api/faucet`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          address
+        })
+      })
+      if (!response.ok) {
+        const errMsg = await response.text()
+        // 如果状态码不在 200-299 范围内，表示出现了错误
+        throw new Error(errMsg);
+      }
+      const res = await response.json()
+      console.log('[ r ] >', res)
+      if (res) {
+        setErrMsg('')
+        setResult(res)
+        console.log('[ res ] >', res)
+      }
+    } catch (error: any) {
+      setErrMsg(error.message)
+    }
+  })
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className={styles.center} />
+      <div className={styles.form}>
+        <h1 style={{ textAlign: 'center' }}>APTOS Faucet</h1>
+        <input
+          style={{height: 30}}
+          value={address}
+          onChange={e => setAddress(e.target.value)}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          style={{ height: 30 }}
+          onClick={handleClick}
+          disabled={isPending}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {isPending ? 'Pending' : 'Get Funds'}
+        </button>
+        {
+          !!result &&
+          <pre className={styles.result}>
+            {
+              JSON.stringify(result, null, 2)
+            }
+          </pre>
+        }
+        <div className={styles.errMsg}>
+          {
+            errMsg
+          }
+        </div>
       </div>
     </main>
   );
